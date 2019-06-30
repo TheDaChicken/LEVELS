904407e3666f9c8710b979cd5179df5bffcba69a
TheDaChicken <steve.rock.pet@gmail.com>
2019-06-29 19:15:22 -0700

    Added a player kill for points.

--- src/level/plugin/Events/onDeath.java
+++ src/level/plugin/Events/onDeath.java
@@ -5,6 +5,7 @@ import level.plugin.Main;
 import level.plugin.Messages;
 import level.plugin.PlayerData;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,28 +19,43 @@ public class onDeath implements Listener {
     }
     @EventHandler
     public void Death(EntityDeathEvent event) {
-        File Config = new File(JavaPlugin.getPlugin(Main.class).getDataFolder().getPath(), "moblistconfig.yml");
-        YamlConfiguration Configcfg = YamlConfiguration.loadConfiguration(Config);
-        File l = new File(JavaPlugin.getPlugin(Main.class).getDataFolder().getPath(), "levelsconfig.yml");
-        YamlConfiguration yml = YamlConfiguration.loadConfiguration(l);
-        if (yml.getBoolean("EnableKillMobsPoints")) {
-            if (Config.exists()) {
-                Player player = event.getEntity().getKiller();
-                String entityName = event.getEntity().getType().getName();
-                if (player != null) {
-                    boolean isThere = Configcfg.get("mobs." + entityName) != null;
-                    if (isThere) {
-                        int points = Configcfg.getInt("mobs." + entityName);
-                        // LivingEntity has a getKiller() method
-                        //entity.getKiller().sendMessage("You killed an entity!");
-                        // getKiller() returns a Player, so you don't have to check if it's a player.
-                        if (Main.playerData.get(player) == null) {
-                            Main.playerData.put(player, new PlayerData(player));
-                        }
-                        try {
-                            Main.playerData.get(player).Addpoints(points);
-                        } catch (MaxLevel maxLevel) {
-                            player.sendMessage(Messages.AddPointsMaxLevelCatchMessage(player));
+        File levels_config = new File(JavaPlugin.getPlugin(Main.class).getDataFolder().getPath(), "levelsconfig.yml");
+        YamlConfiguration yml = YamlConfiguration.loadConfiguration(levels_config);
+        Player player = event.getEntity().getKiller();
+        if (event.getEntity().getType() == EntityType.PLAYER) {
+            if (yml.getBoolean("EnablePlayerPoints")) {
+                int points = yml.getInt("PlayerPointsAmount");
+                if (Main.playerData.get(player) == null) {
+                    Main.playerData.put(player, new PlayerData(player));
+                }
+                try {
+                    Main.playerData.get(player).Addpoints(points);
+                } catch (MaxLevel maxLevel) {
+                    player.sendMessage(Messages.AddPointsMaxLevelCatchMessage(player));
+                }
+            }
+        } else {
+            if (yml.getBoolean("EnableKillMobsPoints")) {
+                File Config = new File(JavaPlugin.getPlugin(Main.class).getDataFolder().getPath(), "moblistconfig.yml");
+                YamlConfiguration Config_cfg = YamlConfiguration.loadConfiguration(Config);
+                if (Config.exists()) {
+                    //Other Mobs
+                    String entityName = event.getEntity().getType().getName();
+                    if (player != null) {
+                        boolean isThere = Config_cfg.get("mobs." + entityName) != null;
+                        if (isThere) {
+                            int points = Config_cfg.getInt("mobs." + entityName);
+                            // LivingEntity has a getKiller() method
+                            //entity.getKiller().sendMessage("You killed an entity!");
+                            // getKiller() returns a Player, so you don't have to check if it's a player.
+                            if (Main.playerData.get(player) == null) {
+                                Main.playerData.put(player, new PlayerData(player));
+                            }
+                            try {
+                                Main.playerData.get(player).Addpoints(points);
+                            } catch (MaxLevel maxLevel) {
+                                player.sendMessage(Messages.AddPointsMaxLevelCatchMessage(player));
+                            }
                         }
                     }
                 }
