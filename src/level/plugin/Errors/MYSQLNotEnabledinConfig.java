7dc826ec92c245629d2eb0cd2d1bb77061319439
TheDaChicken <steve.rock.pet@gmail.com>
2019-10-31 16:05:05 -0700

    Remove everything to start recoding.

--- src/level/plugin/Errors/MYSQLNotEnabledinConfig.java
+++
@@ -1,7 +0,0 @@
-package level.plugin.Errors;
-
-public class MYSQLNotEnabledinConfig extends Exception {
-    public MYSQLNotEnabledinConfig() {
-        super("MYSQL not enabled in the config!");
-    }
-}
