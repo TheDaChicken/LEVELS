9b14719a0e828093344e59adb4590e54905162a0
TheDaChicken <steve.rock.pet@gmail.com>
2019-11-02 13:07:39 -0700

    Create PlayerData class.

---
+++ src/level/plugin/PlayerData.java
@@ -0,0 +1,16 @@
+package level.plugin;
+
+import org.bukkit.entity.Player;
+
+public class PlayerData {
+
+    public PlayerData(String username) {
+
+    }
+
+    public PlayerData(Player player) {
+        
+
+    }
+
+}
