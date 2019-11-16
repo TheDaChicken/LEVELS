416438bc0f20768a798e3e53bd9b73b9019c1f01
TheDaChicken <steve.rock.pet@gmail.com>
2019-11-16 11:06:31 -0800

    Added commands per level back.

--- src/level/plugin/Events/LevelUpListener.java
+++ src/level/plugin/Events/LevelUpListener.java
@@ -3,10 +3,13 @@ package level.plugin.Events;
 import level.plugin.CustomEvents.LevelUpEvent;
 import level.plugin.CustomJavaPlugin;
 import level.plugin.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.List;
 
 public class LevelUpListener implements Listener {
 
@@ -17,6 +20,24 @@ public class LevelUpListener implements Listener {
 
         CustomJavaPlugin javaPlugin = CustomJavaPlugin.getPlugin(Main.class);
         FileConfiguration ymlconfig = javaPlugin.getConfig();
+
+        if (ymlconfig.getBoolean("levelupruncommand")) {
+            if (ymlconfig.getBoolean("levelupruncommandcertainperlevel")) {
+                if (ymlconfig.getStringList("levelcommands." + level) == null) {
+                    return;
+                }
+                List<String> commands = ymlconfig.getStringList("levelcommands." + level);
+                for (String command : commands) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", player.getName()));
+                }
+            } else {
+                List<String> commands = ymlconfig.getStringList("levelupruncommandslist");
+                for (String command : commands) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", player.getName()));
+                }
+            }
+        }
+
     }
 
 }
